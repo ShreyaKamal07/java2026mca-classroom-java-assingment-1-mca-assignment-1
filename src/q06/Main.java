4e618345d4c1cb98578443cf0f2178d17a2d68c9
Shreya Kamal <shreya22004@gmail.com>
2026-04-07 11:10:15 +0530

    Update Main.java

--- src/q06/Main.java
+++ src/q06/Main.java
@@ -1,3 +1,4 @@
+package.q06;
 import java.util.*;
 
 public class Main {
@@ -5,11 +6,23 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // TODO: Print first n Fibonacci numbers separated by spaces
-        //       F(0)=0, F(1)=1, F(n)=F(n-1)+F(n-2)
-        //
-        // Input: 8
-        // Output: 0 1 1 2 3 5 8 13
+        int a = 0, b = 1;
 
+        for (int i = 0; i < n; i++) {
+            // print number
+            System.out.print(a);
+
+            // avoid trailing space
+            if (i < n - 1) {
+                System.out.print(" ");
+            }
+
+            // next fibonacci
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+
+        sc.close();
     }
 }
