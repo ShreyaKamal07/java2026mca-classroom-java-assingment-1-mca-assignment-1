80b5345bed5ba7aecfc6d867d58b4762d5742155
Shreya Kamal <shreya22004@gmail.com>
2026-04-07 10:59:21 +0530

    Update Main.java

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -1,3 +1,4 @@
+package.q02;
 import java.util.*;
 
 public class Main {
@@ -5,17 +6,24 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // TODO: Print Pascal's Triangle for n rows
-        //       Values on each row separated by single space
-        //       No leading or trailing spaces
-        //
-        // Input: 5
-        // Output:
-        // 1
-        // 1 1
-        // 1 2 1
-        // 1 3 3 1
-        // 1 4 6 4 1
+        for (int i = 0; i < n; i++) {
+            int num = 1;
 
+            for (int j = 0; j <= i; j++) {
+                // print number
+                System.out.print(num);
+
+                // avoid trailing space
+                if (j < i) {
+                    System.out.print(" ");
+                }
+
+                // calculate next value
+                num = num * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
