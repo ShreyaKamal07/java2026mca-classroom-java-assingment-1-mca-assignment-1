5db4af211bec9991035c06073dc46ce244b93f72
Shreya Kamal <shreya22004@gmail.com>
2026-04-07 10:46:17 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -1,25 +1,49 @@
-import java.util.*;
+package.q01
+import java.util.Scanner;
 
-public class Main {
+public class HollowDiamond {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = sc.nextInt(); // half height
 
-        // TODO: Print a hollow diamond pattern of half-height n using '*'
-        //
-        // Rules:
-        //   - Only border cells have '*', inside is spaces
-        //   - No trailing spaces on any line
-        //
-        // Input: 4
-        // Output:
-        //    *
-        //   * *
-        //  *   *
-        // *     *
-        //  *   *
-        //   * *
-        //    *
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            // spaces before
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
 
+            // stars and spaces
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+            // spaces before
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            // stars and spaces
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
