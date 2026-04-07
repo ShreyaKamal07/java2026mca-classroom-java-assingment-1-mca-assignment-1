7575a52dcc9aa721d633537c3657932ecabcef63
Shreya Kamal <shreya22004@gmail.com>
2026-04-07 11:02:42 +0530

    Update Main.java

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -1,3 +1,4 @@
+package.q03;
 import java.util.*;
 
 public class Main {
@@ -5,16 +6,61 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
-        //       Print each row with values separated by single space
-        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
-        //
-        // Input: 3
-        // Output:
-        // 1 2 3
-        // 8 9 4
-        // 7 6 5
-        // Diagonal: 15
+        int[][] mat = new int[n][n];
 
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+
+        // Fill spiral matrix
+        while (top <= bottom && left <= right) {
+
+            // left → right
+            for (int i = left; i <= right; i++) {
+                mat[top][i] = num++;
+            }
+            top++;
+
+            // top → bottom
+            for (int i = top; i <= bottom; i++) {
+                mat[i][right] = num++;
+            }
+            right--;
+
+            // right → left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    mat[bottom][i] = num++;
+                }
+                bottom--;
+            }
+
+            // bottom → top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    mat[i][left] = num++;
+                }
+                left++;
+            }
+        }
+
+        // Print matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(mat[i][j]);
+                if (j < n - 1) System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        // Diagonal sum
+        int diagSum = 0;
+        for (int i = 0; i < n; i++) {
+            diagSum += mat[i][i];
+        }
+
+        System.out.println("Diagonal: " + diagSum);
+
+        sc.close();
     }
 }
